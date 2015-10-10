d226bf96277bf4d3e514fbdda2b5530ae2b657ec
pengtao.geng <pengtao.geng@qunar.com>
2015-10-10 11:23:08 +0800

    modify

--- src/test/groovy/com/gpengtao/test/TestGroovy.groovy
+++ src/test/groovy/com/gpengtao/test/TestGroovy.groovy
@@ -1,11 +1,15 @@
+package com.gpengtao.test
+
 import com.gpengtao.model.Person
+import org.junit.Test
 
 /**
  * Created by pengtao.geng on 2015/10/10.
  */
 class TestGroovy {
 
-    static void main(args) {
+    @Test
+    void test() {
         def person = new Person(name: "gpengtao", age: 20, email: "123@qq.com")
 
         println person
